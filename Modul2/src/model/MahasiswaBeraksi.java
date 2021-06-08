51575cef8ac2704aab917fe2eb5427399d2d4064
MuhammadRizqiYusuf <rizqiyusuf003@gmail.com>
2021-06-08 19:57:33 +0800

    Membuat Class, Atribut, dan Objek

---
+++ Modul2/src/model/MahasiswaBeraksi.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.npm = "19630306";
+        mahasiswa.nama = "Muhammad Rizqi Yusuf";
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("18-01-2001");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.npm = "19630307";
+        mahasiswa2.nama = "Rayhan Azmi";
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("27-03-2001");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+    }
+}
