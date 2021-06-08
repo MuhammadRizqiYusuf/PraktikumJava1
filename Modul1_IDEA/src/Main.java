bc63886a163b7d624abc2372c1fcb6caafc820cc
MuhammadRizqiYusuf <rizqiyusuf003@gmail.com>
2021-06-08 18:58:55 +0800

    Project IDEA

---
+++ Modul1_IDEA/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Muhammad ";
+        String namaTengah = "Rizqi ";
+        String namaBelakang = "Yusuf";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.897654512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
