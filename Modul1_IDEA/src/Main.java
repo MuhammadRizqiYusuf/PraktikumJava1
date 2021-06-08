53ffb980982a9656dc8705307346df1f119d64ea
MuhammadRizqiYusuf <rizqiyusuf003@gmail.com>
2021-06-08 19:22:24 +0800

    Operasi Tipe String

--- Modul1_IDEA/src/Main.java
+++ Modul1_IDEA/src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        String namaDepan = "Muhammad ";
-        String namaTengah = "Rizqi ";
+        String namaDepan = "Muhammad";
+        String namaTengah = "Rizqi";
         String namaBelakang = "Yusuf";
         int usia = 18;
         int targetTahunKuliah = 4;
@@ -14,5 +16,21 @@ public class Main {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace("qi","ka"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
