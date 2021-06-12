0f3ca99f8228adfe2579ee60d18e2dad321a455d
MuhammadRizqiYusuf <rizqiyusuf003@gmail.com>
2021-06-12 23:08:45 +0800

    Membuat class GuiByText

---
+++ Modul3/src/GuibyText.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class GuibyText extends JFrame{
+    GuibyText(){
+        setSize(640, 480);
+    }
+
+    public static void main(String[] args) {
+        GuibyText guibyText = new GuibyText();
+        guibyText.setVisible(true);
+    }
+}
